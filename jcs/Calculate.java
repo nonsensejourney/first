ce21404933a573bf3b2cdc69d3e65fe58cb3fd67
nonsensejourney <brauthumean@gmail.com>
2018-07-03 03:00:43 +0400

    other operations are implemented

--- jcs/Calculate.java
+++ jcs/Calculate.java
@@ -4,6 +4,10 @@ System.out.println("Calculate...");
 int first = Integer.valueOf(arg[0]);
 int second = Integer.valueOf(arg[1]);
 int summ = first + second;
-System.out.println("Sum is "+ summ);
+int sub12 = first - second; 
+int mult = first * second;
+int div12 = first / second;  
+int div21 = second / first; 
+System.out.println("Sum is "+ summ +" Sub is "+sub12 + " Mult is "+mult+ " Div is "+div12);
 }
 }
