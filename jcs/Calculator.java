7b007bd3cbef6d3d94011e0e0107f6b2dde4d4a1
nonsensejourney <brauthumean@gmail.com>
2018-07-06 10:10:31 +0400

    functionality is enhanced

--- jcs/Calculator.java
+++ jcs/Calculator.java
@@ -3,24 +3,65 @@
 public class Calculator{
 	/**
 	*/
-	private int result;
+	private static int result;
 	/**
+	general action
 	*/
-	public void add(int... prmtr){
-		for (Integer param:prmtr){
-			this.result+=param;
+	public static void act(String choice, int... prmtr){
+		if (choice.equals("add")){
+			Calculator.add(prmtr);
+		}
+		else if (choice.equals("sub")){
+			Calculator.sub(prmtr);
+		}
+		else if (choice.equals("mul")){
+			Calculator.mul(prmtr);
+		}
+		else if (choice.equals("div")){
+			Calculator.div(prmtr);
 		}
 	}
+		/**
+	addition
+	*/
+	public static void add(int... prmtr){
+		Calculator.result=prmtr[0]+prmtr[1];
+	}
+		
+	/**
+	subtraction
+	*/
+	public static void sub(int... prmtr){
+		Calculator.result=prmtr[0]-prmtr[1];
+	}
+	
+	/**
+	multiplication
+	*/
+	public  static void mul(int... prmtr){
+		Calculator.result=prmtr[0]*prmtr[1];
+	}
+	
+	/**
+	division
+	*/
+	public static  void div(int... prmtr){
+		Calculator.result=prmtr[0]/prmtr[1];
+	}
+	
 	/**
 	@return result of calculation
 	*/
-	public int getResult(){
-		return this.result;
+	public static int getResult(){
+		return Calculator.result;
 	}
+	
 	/**
 	clean result of calculation
 	*/
-	public void cleanResult(){
-		this.result=0;
+	public static void cleanResult(){
+		Calculator.result=0;
 	} 
+	
+	
 }
--- jcs/InteractRunner.java
+++ jcs/InteractRunner.java
@@ -1,28 +1,142 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.System.*;
 
 /**
-starting Calvulatir, providing input user's data
+ 
 */
 public class InteractRunner{
+	
+	    static int numberOfOperations = 0;               // shows nuber of operations
+		static String serial = "default";                       // shows if previos result will be used in next operation
+		static String choice = "not chosen";             // ralated to what exactly operation is
+		static String exit = "n";                        // "no" or "yes" as answer on question: are you going to exit?
+		static String firstArgument = "default";         // first agrument		
+		static String secondArgument = "default";        // first agrument
+		static int[] argmnts = {0,0};                        // array of int agruments
+    	                       
+		//* operation keys */
+		static String[] addK={"first addend","second addend","Sum"};
+		static String[] subK={"minuend","subtrahend","Difference"};
+		static String[] mulK={"multiplicand","multiplier","Product"};
+		static String[] divK={"dividend","divisor","Quotient"};
+		static String[] keys = {"default","default","default"};
+	
 	public static void main(String[] arg){
-		Scanner reader = new Scanner (System.in);
-		try{
-			Calculator calc = new Calculator();
-			String exit ="n";
-			while (!exit.equals("y")){
-				System.out.println("Enter first argument: ");
-				String first = reader.next();
-				System.out.println("Enter second argument: ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first),Integer.valueOf(second));
-				System.out.println("Result is "+calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit:y/n");
-				exit=reader.next();
-			}
+		
+				
+		//* main instructions */
+		System.out.println("Hello!");
+		while(!exit.equals("y")){
+			Check.chooseOperationorQuit();
+			Data.input();
+			Calculator.act(choice, argmnts);
+			Data.output();
+			numberOfOperations++;
+			Check.exit();			
 		}
-			finally{
-				reader.close();
-			}
-		}
+		
 	}
+	//* io data*/
+	static class Data{
+		String datain = "default";
+		static Scanner inreader = new Scanner (System.in);
+		
+        static void output(){
+			System.out.println("____________________");
+			System.out.println("");
+			System.out.println("Result is "+Calculator.getResult());
+			System.out.println("");
+		}		
+		
+		static void input(){
+			
+			InteractRunner.Data.setKeys();
+			
+        if (InteractRunner.numberOfOperations==0){
+			System.out.println("Enter "+keys[0]+": ");
+			InteractRunner.firstArgument = inreader.next();
+			System.out.println("Enter "+keys[1]+": ");
+			InteractRunner.secondArgument = inreader.next();
+			argmnts[0]=Integer.valueOf(InteractRunner.firstArgument);
+			argmnts[1]=Integer.valueOf(InteractRunner.secondArgument);
+		}
+        else{		
+		
+		    if (InteractRunner.serial.equals("n")) {
+			   System.out.println("Enter "+keys[0]+": ");
+			   InteractRunner.firstArgument = inreader.next();
+			   System.out.println("Enter "+keys[1]+": ");
+			   InteractRunner.secondArgument = inreader.next();
+			   argmnts[0]=Integer.valueOf(InteractRunner.firstArgument);
+			   argmnts[1]=Integer.valueOf(InteractRunner.secondArgument);
+		}
+		    else if (InteractRunner.serial.equals("y")) {
+			        System.out.println("Enter "+keys[1]+": ");
+			        InteractRunner.secondArgument = inreader.next();
+					InteractRunner.firstArgument = Integer.toString(Calculator.getResult());
+					argmnts[0]=Calculator.getResult();
+			        argmnts[1]=Integer.valueOf(InteractRunner.secondArgument);
+		}
+		}	
+		}	
+		        static void setKeys(){
+				     if (InteractRunner.choice.equals("add")) {System.arraycopy(InteractRunner.addK,0,InteractRunner.keys,0,2);}
+				else if (InteractRunner.choice.equals("sub")) {System.arraycopy(InteractRunner.subK,0,InteractRunner.keys,0,2);}
+				else if (InteractRunner.choice.equals("mul")) {System.arraycopy(InteractRunner.mulK,0,InteractRunner.keys,0,2);}
+				else if (InteractRunner.choice.equals("div")) {System.arraycopy(InteractRunner.divK,0,InteractRunner.keys,0,2);}
+				}
+				
+		}
+		
+	
+	//* action manager */
+	static class Check{
+		static String answer = "default";
+		static void chooseOperationorQuit(){
+			//String choice = "default";
+			
+			Scanner creader = new Scanner (System.in);
+			System.out.println("Choose operation or quit: add/sub/mul/div/q");
+			choice = creader.next();
+			
+			if(choice.equals("q")) {
+				Calculator.cleanResult();
+				InteractRunner.exit="y";
+                System.out.println("Googbye!"); 				
+				}
+			else if (choice.equals("add")){
+				InteractRunner.choice="add";				
+			}
+			else if (choice.equals("sub")){
+				InteractRunner.choice="sub";
+			}
+			else if (choice.equals("mul")){
+				InteractRunner.choice="mul";
+			}
+			else if (choice.equals("div")){
+				InteractRunner.choice="div";
+			}
+		}
+		static void exit(){
+			Scanner areader = new Scanner (System.in);
+			System.out.println("Exit? y/n");
+			answer = areader.next();
+			
+			if (answer.equals("y")){
+				Calculator.cleanResult();
+				InteractRunner.exit = InteractRunner.Check.answer;
+                System.out.println("Googbye!"); 				
+			}
+			else if (answer.equals("n")){
+				InteractRunner.Check.serial();
+			}
+		}
+	
+	    static void serial(){
+			Scanner sreader = new Scanner (System.in);
+			System.out.println("Are you going to use previuos result as first argument in next calculation? y/n");
+			InteractRunner.serial = sreader.next();
+		}
+	}		
+}
+	
